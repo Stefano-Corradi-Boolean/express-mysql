1501770cff65125960cee7cc3f835484361396a9
ugodeughi <stefanocorradi@gmail.com>
2025-02-05 12:03:59 +0100

    show join 1

--- controllers/pizzaController.js
+++ controllers/pizzaController.js
@@ -3,13 +3,50 @@
 const connection = require('../data/db')
 
 const index = (req, res) => {
-  res.send('Elenco pizze');
+  const sql = 'SELECT * FROM pizzas';
+
+  // effettuo la query al db
+  connection.query(sql, (err, results) => {
+    if (err) return res.status(500).json({ error: 'Query al database fallita' })
+    res.json(results);
+  })
+
+
 }
 
 const show = (req, res) => {
 
   const id = req.params.id;
-  res.send(`Dettaglio pizza ${id}`);
+
+  const sqlPizza = 'SELECT * FROM pizzas WHERE id = ?';
+
+  const sqlIngrediets = `SELECT I.*
+  FROM ingredients I
+  JOIN ingredient_pizza IP ON IP.ingredient_id = I.id
+  WHERE IP.pizza_id = ?
+  `
+
+  connection.query(sqlPizza, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: 'Query al database fallita' })
+    // se non trovo la pizza restituisco un errore 500
+    if (results.length === 0) return res.status(404).json({ error: 'Pizza non trovata' });
+
+    // superati i controlli di errore e 404 restituisco la prima occorrenza dell'array
+    let pizza = results[0];
+
+    // effettuo la query per gli ingredienti
+    connection.query(sqlIngrediets, [id], (err, ingredientsResult) => {
+
+      if (err) return res.status(500).json({ error: 'Query al database fallita' })
+      pizza.ingredients = ingredientsResult;
+
+      res.json(pizza);
+    })
+
+
+
+  })
+
 }
 
 //store
@@ -32,7 +69,12 @@ const modify = (req, res) => {
 //delete
 const destroy = (req, res) => {
   const id = req.params.id;
-  res.send(`Eliminazione pizza ${id}`);
+  const sql = 'DELETE FROM pizzas WHERE id = ?';
+
+  connection.query(sql, [id], (err) => {
+    if (err) return res.status(500).json({ error: 'Eliminazione della pizza fallita' });
+    res.sendStatus(204);
+  })
 }
 
 module.exports = {
